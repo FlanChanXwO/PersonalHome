3b9a843693df5bf2a2b8389e9f042608f7de372f
FlanChanXwO <nepnepnepnep2002@gmail.com>
2022-05-16 18:13:36 +0800

    Initial commit

---
+++ src/main/java/org/flanzone/PHApplication.java
@@ -0,0 +1,13 @@
+package org.flanzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PHApplication.class, args);
+    }
+
+}
