8f06e34ca512d8a4d9c3cb4e35459685917396de
FlanChan <nepnepnepnep2002@gmail.com>
2022-06-13 17:40:40 +0800

    FlanChan Second Commit

--- src/main/java/org/flanzone/HomeApp.java
+++ src/main/java/org/flanzone/HomeApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeApp {
-
     public static void main(String[] args) {
         SpringApplication.run(HomeApp.class, args);
     }
